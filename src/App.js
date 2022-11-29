22c3792f0dae98cfc37e02946a2f2ccc92a55a4b
Marbowle <mahomegol@gmail.com>
2022-11-29 15:32:43 +0100

    Built initial strusture for search form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-
-import './App.css';
-
+import "./App.css";
+import Dictionary from "./Dictionary";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      <header className="App-header"></header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer>
+        This code was coded by{" "}
+        <a
+          href="https://martadomagala.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Marta Domagała
+        </a>{" "}
+        and it's{" "}
+        <a
+          href="https://github.com/Marbowle/dictionary-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced
+        </a>
+        .
+      </footer>
     </div>
   );
 }
